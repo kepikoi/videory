77a0de5a9ff3c89e30d8eb6c1cc12fa320ca2838
Alec Utin <kepi@binks.rocks>
2020-02-22 19:41:37 +0100

    Support .mov container input

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 
 const
-    db = require('./db')
-    , {findAndUpdate} = require('./hound')
-    , searchExt = 'MP4'
-    , {logAndExit} = require("./helpers")
+    db = require("./db")
+    , { findAndUpdate } = require("./hound")
+    , searchExt = ["MP4", "MOV"]
+    , { logAndExit } = require("./helpers")
     , transcoder = require("./transcode")
-    , {removeStalledTranscodings} = require("./db")
+    , { removeStalledTranscodings } = require("./db")
     , args = require("args")
 ;
 
 args
-    .option('in', 'directory transcode videos from')
-    .option('out', 'directory to transcode videos to')
-    .option('allow-versions', 'creates a file version if output file does already exist')
+    .option("in", "directory transcode videos from")
+    .option("out", "directory to transcode videos to")
+    .option("allow-versions", "creates a file version if output file does already exist")
 ;
 
 const flags = args.parse(process.argv);
@@ -27,21 +27,21 @@ if (!d_out) {
     throw  new Error("missing 'out' flag");
 }
 if (flags.allowVersions) {
-    global.allowVersions = true
+    global.allowVersions = true;
 }
 
 (async function () {
     //init sqlite
     await db.init()
         .catch(logAndExit(1));
-
+    
     //remove videos that started to transcode and did not finish for some reason
     await removeStalledTranscodings();
-
+    
     //initially search fs for videos
     await findAndUpdate([d_in], searchExt)
         .catch(logAndExit(2));
-
+    
     //start transcoder
-    transcoder.schedule(d_out).next()
+    transcoder.schedule(d_out).next();
 })();
